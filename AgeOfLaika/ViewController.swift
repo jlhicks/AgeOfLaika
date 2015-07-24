e6698c278df4cf983f3c0b0affa1e7571e56c776
James Hicks <jlh.zorch@gmail.com>
2015-07-23 19:47:29 -0700

    Add actual code.

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var humanYears: UITextField!
+  @IBOutlet weak var dogYears: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  @IBAction func convertToDogYears(sender: UIButton) {
+    let humanToDogYearsMultiplier = 7
+    
+    humanYears.resignFirstResponder()
+    let humanAge = humanYears.text.toInt()!
+    humanYears.text = ""
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    dogYears.text = "\(humanAge) human years is \(humanAge * 7) in dog years."
+    dogYears.hidden = false
+  }
+  
+  @IBAction func convertToRealDogYears(sender: UIButton) {
+    let youngerDogMultiplier = 10.5
+    let olderDogMultiplier = 4.0
+    let dogAgeCutoff = 2.0
+    var dogAge:Double
+    
+    humanYears.resignFirstResponder()
+    let humanAge = Double(humanYears.text.toInt()!)
+    humanYears.text = ""
+    
+    if humanAge <= dogAgeCutoff {
+      dogAge = humanAge * youngerDogMultiplier
+    } else {
+      dogAge = (humanAge - dogAgeCutoff) * olderDogMultiplier + dogAgeCutoff * youngerDogMultiplier
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    dogYears.text = "\(humanAge) human years is \(dogAge) in dog years."
+    dogYears.hidden = false
+  }
 }
 
